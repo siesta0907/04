ca688d5bb1aa6c04acc2666ed142824cb1b37863
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-09-27 16:20:49 +0900

    실습3

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 //04 실습 
 //IT공학전공 1916513 박희수 
 //main.c
-//실습1 
+//실습3
 
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-	int x, y;
-	printf("두 개의 정수를 입력하세요: ");
-	scanf("%i %i", &x ,&y);
-	
-	printf("%d + %d = %d\n",x, y, x+y);
-	printf("%d - %d = %d\n",x, y, x-y);
-	printf("%d * %d = %d\n",x, y, x*y);
-	printf("%d / %d = %d\n",x, y, x/y);
-	printf("%d %% %d = %d\n",x, y, x%y);
+	int time;
+	int min, sec;
+	printf("초를 입력하세요: ");
+	scanf("%i", &time);
+	min = time /60;
+	sec = time%60;
+	printf("%d:%d",min,sec);
 	return 0;
 }
