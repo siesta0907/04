09f74d5546f1b8fa56bb7cf054a5e9f42ea83a91
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-09-27 16:31:58 +0900

    실습5

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 //04 실습 
 //IT공학전공 1916513 박희수 
 //main.c
-//실습4
+//실습5
 
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-	int year;
-	printf("년도를 입력하세요: ");
-	scanf("%i", &year);
-	printf("%d", year%4 == 0 && year%100 != 0 || year %400 ==0);
+	unsigned int x;
+	int b;
+	printf("숫자를 입력하세요: ");
+	scanf("%ui", &x);
+	for (b = 0; x !=0; x>>=1){
+		if (x&1){
+			b++;
+		}
+	}
+	printf("결과: %i", b);
 
 	return 0;
 }
