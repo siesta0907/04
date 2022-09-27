e1c71108566bccbf828e98bc7333b03ecaca4404
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-09-27 16:25:03 +0900

    실습4

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 //04 실습 
 //IT공학전공 1916513 박희수 
 //main.c
-//실습3
+//실습4
 
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-	int time;
-	int min, sec;
-	printf("초를 입력하세요: ");
-	scanf("%i", &time);
-	min = time /60;
-	sec = time%60;
-	printf("%d:%d",min,sec);
+	int year;
+	printf("년도를 입력하세요: ");
+	scanf("%i", &year);
+	printf("%d", year%4 == 0 && year%100 != 0 || year %400 ==0);
+
 	return 0;
 }
