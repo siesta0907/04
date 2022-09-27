4ec190bd484ff011c43671aaae34b6916e323184
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-09-27 16:34:47 +0900

    실습6

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 //04 실습 
 //IT공학전공 1916513 박희수 
 //main.c
-//실습5
+//실습6
 
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-	unsigned int x;
-	int b;
-	printf("숫자를 입력하세요: ");
-	scanf("%ui", &x);
-	for (b = 0; x !=0; x>>=1){
-		if (x&1){
-			b++;
-		}
-	}
-	printf("결과: %i", b);
-
+	int time;
+	int hour, min, sec;
+	printf("초를 입력하세요: ");
+	scanf("%i", &time);
+	hour = time/3600;
+	min = time%3600/60;
+	sec = time%60;
+	printf("%d:%d:%d",hour,min,sec);
 	return 0;
 }
